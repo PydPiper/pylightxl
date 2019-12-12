0fc290daa586213879c7b22970302a7457b60aed
PydPiper <vkisforever@gmail.com>
2019-12-11 22:50:06 -0800

    added commond string lookup file, cleaned up reader and added val lookup for string

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# lightxl
-A light weight, zero dependency, minial functionality excel read/writer python library
+# pylightxl
+A light weight, zero dependency, minial functionality excel read/writer python library. 
+Supports .xlsx and .xlsm file extensions. Does not support .xls (excel 97-2003 worksheet).
--- test.py
+++ test.py
@@ -20,6 +20,19 @@ with file_zip.open('xl/workbook.xml', 'r') as f:
         # simply index to 1 to get the sheet name: Sheet1
         SHEET_NAMES.append(sheet_text_line.split('"')[1])
 
+SHARED_STRINGS = {}
+
+# check for sharedString.xml file that contains string values of cells
+if 'xl/sharedStrings.xml' in file_zip.NameToInfo.keys():
+    with file_zip.open('xl/sharedStrings.xml') as f:
+        text = f.read()
+        text = text.decode()
+
+        tag_t = re.compile(r'<t>(.*?)</t>')
+        tag_t_vals = tag_t.findall(text)
+        for i, val in enumerate(tag_t_vals):
+            SHARED_STRINGS.update({i: val})
+
 # the .zip file worksheets are all start Sheet1, Sheet2... names, pull out all the files names that contain "sheet"
 zip_sheetnames = [name for name in file_zip.NameToInfo.keys() if 'sheet' in name]
 
@@ -35,18 +48,27 @@ for i, zip_sheetname in enumerate(zip_sheetnames):
         tag_sheetdata = re.compile(r'\<sheetData.*sheetData\>')
         sheetdata_section = tag_sheetdata.findall(text)[0]
         tag_cr = re.compile(r'<c r=')
-        parts = tag_cr.split(sheetdata_section)[1:]
-        for part in parts:
-            subparts = part.split('>')
-            cell = subparts[0]
-            # remove: </v
-            val = subparts[2][:-3]
+        tag_cr_lines = tag_cr.split(sheetdata_section)[1:]
+        for tag_cr_line in tag_cr_lines:
+            # pull out cell address and test if it's a string cell that needs lookup
+            re_cell_address = re.compile(r'[^<r c="][^"]+')
+            finding_cell_address = re_cell_address.findall(tag_cr_line)
+            cell_address = finding_cell_address[0]
+            cell_string = True if 't="s"' in tag_cr_line else False
+
+            re_cell_val = re.compile(r'(?<=<v>)(.*)(?=</v>)')
+            cell_val = re_cell_val.findall(tag_cr_line)[0]
+
+            if cell_string is True:
+                cell_val = SHARED_STRINGS[int(cell_val)]
+
+            # add to db
             existing_datadict = DATABASE[SHEET_NAMES[i]]
-            existing_datadict.update({cell: val})
+            existing_datadict.update({cell_address: cell_val})
             DATABASE.update({SHEET_NAMES[i]: existing_datadict})
         pass
 
 # convert binary to string
 
-
+print(DATABASE)
 pass
