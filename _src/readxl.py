e73d26397fe9908517b8008fb721c635c2958a9a
PydPiper <vkisforever@gmail.com>
2019-12-13 18:34:12 -0800

    finished builing worksheet class, added a bunch of indexing features

--- _src/readxl.py
+++ _src/readxl.py
@@ -1,63 +1,245 @@
 import zipfile
 import re
 from functools import reduce
+from os.path import isfile
 
-class Database():
+
+class Database:
 
     def __init__(self):
-        self.worksheet = {}
+        self._worksheet = {}
 
     def __repr__(self):
         return 'pylightxl.Database'
 
+    def worksheet(self, sheetname):
+        """
+        Indexes worksheets within the database
+        :param str sheetname: worksheet name
+        :return: pylightxl.Database.Worksheet class object
+        """
+
+        try:
+            return self._worksheet[sheetname]
+        except KeyError:
+            raise ValueError('Error - Sheetname ({}) is not in the database'.format(sheetname))
+
     @property
     def worksheetnames(self):
-        return list(self.worksheet.keys())
+        """
+        Returns a list of database stored worksheet names
+        :return: list of worksheet names
+        """
+
+        return list(self._worksheet.keys())
+
+    def add_worksheet(self, sheetname, data):
+        """
+        Logs worksheet name and its data in the database
+        :param str sheetname: worksheet name
+        :param data: dictionary of worksheet cell values (ex: {'A1': 10, 'A2': 20})
+        :return: None
+        """
+
+        self._worksheet.update({sheetname: Worksheet(data)})
 
 
-class Worksheet():
+class Worksheet:
 
-    def __init__(self):
-        self.data = {}
+    def __init__(self, data):
+        self._data = data
+        self.maxrow = 0
+        self.maxcol = 0
+        self._calc_size()
 
-    @property
-    def maxrow(self):
-        pass
+    def __repr__(self):
+        return 'pylightxl.Database.Worksheet'
 
     def _calc_size(self):
-        list_of_addresses = list(self.data.keys()).sort()
-        largest_col_address = list_of_addresses[-1]
-        col = self.address2index(largest_col_address)[1]
-        #TODO: finish calc-ing row
-        pass
+        """
+        Calculates the size of the worksheet row/col. This only occurs on initialization
+        :return: None (but this creates instance attributes maxrow/maxcol)
+        """
+
+        if self._data != {}:
+            list_of_addresses = list(self._data.keys())
+            list_of_addresses.sort()
+            largest_col_address = list_of_addresses[-1]
+            self.maxcol = self.address2index(largest_col_address)[1]
+
+            strVSnum = re.compile(r'[A-Z]+')
+            list_of_rows = [int(strVSnum.split(address)[1]) for address in list_of_addresses]
+            self.maxrow = max(list_of_rows)
+        else:
+            self.maxrow = 0
+            self.maxcol = 0
 
     @property
     def size(self):
+        """
+        Returns the size of the worksheet (row/col)
+        :return: list of [maxrow, maxcol]
+        """
+
         return [self.maxrow, self.maxcol]
 
-    def address(self, range):
-        pass
+    def address(self, address):
+        """
+        Takes an excel address and returns the worksheet stored value
+        :param str address: Excel address (ex: "A1")
+        :return: cell value
+        """
+
+        try:
+            rv = self._data[address]
+        except KeyError:
+            # no data was parsed, return empty cell value
+            rv = ""
+
+        return rv
 
     def index(self, row, col):
-        pass
+        """
+        Takes an excel row and col starting at index 1 and returns the worksheet stored value
+        :param int row: row index (starting at 1)
+        :param int col: col index (start at 1 that corresponds to column "A")
+        :return: cell value
+        """
+
+        address = self.index2address(row,col)
+        try:
+            rv = self._data[address]
+        except KeyError:
+            # no data was parsed, return empty cell value
+            rv = ""
+
+        return rv
+
+    def row(self, row):
+        """
+        Takes a row index input and returns a list of cell data
+        :param int row: row index (starting at 1)
+        :return: list of cell data
+        """
+
+        rv = []
+
+        for c in range(1, self.maxcol + 1):
+            val = self.index(row, c)
+            rv.append(val)
+
+        return rv
+
+    def col(self, col):
+        """
+        Takes a col index input and returns a list of cell data
+        :param int col: col index (start at 1 that corresponds to column "A")
+        :return: list of cell data
+        """
+
+        rv = []
+
+        for r in range(1, self.maxrow + 1):
+            val = self.index(r, col)
+            rv.append(val)
+
+        return rv
+
+    @property
+    def rows(self):
+        """
+        Returns a list of rows that can be iterated through
+        :return: list of rows-lists (ex: [[11,12,13],[21,22,23]] for 2 rows with 3 columns of data
+        """
+
+        rv = []
+
+        for r in range(1, self.maxrow + 1):
+            rv.append(self.row(r))
+
+        return rv
+
+    @property
+    def cols(self):
+        """
+        Returns a list of cols that can be iterated through
+        :return: list of cols-lists (ex: [[11,21],[12,22],[13,23]] for 2 rows with 3 columns of data
+        """
+
+        rv = []
+
+        for c in range(1, self.maxcol + 1):
+            rv.append(self.col(c))
+
+        return rv
 
     def address2index(self, address):
+        """
+        Convert excel address to row/col index
+        :param str address: Excel address (ex: "A1")
+        :return: list of [row, col]
+        """
+        if type(address) is not str:
+            raise ValueError('Error - Address ({}) must be a string.'.format(address))
+        if address == '':
+            raise ValueError('Error - Address ({}) cannot be an empty str.'.format(address))
+
+        address = address.upper()
+
         strVSnum = re.compile(r'[A-Z]+')
-        colstr = strVSnum.findall(address)[0]
-        col = reduce(lambda x,y: ord(x)-64+ord(y)-64, colstr)
-        row = int(strVSnum.split(address)[0])
-        return [row,col]
+        try:
+            colstr = strVSnum.findall(address)[0]
+        except IndexError:
+            raise ValueError('Error - Incorrect address ({}) entry. Address must be an alphanumeric '
+                             'where the starting character(s) are alpha characters a-z'.format(address))
+
+        if not colstr.isalpha():
+            raise ValueError('Error - Incorrect address ({}) entry. Address must be an alphanumeric '
+                             'where the starting character(s) are alpha characters a-z'.format(address))
+
+        if len(colstr) == 1:
+            col = ord(colstr) - 64
+        else:
+            col = reduce(lambda x, y: ord(x)-64+ord(y)-64, colstr)
+
+        try:
+            row = int(strVSnum.split(address)[1])
+        except (IndexError, ValueError):
+            raise ValueError('Error - Incorrect address ({}) entry. Address must be an alphanumeric '
+                             'where the trailing character(s) are numeric characters 1-9'.format(address))
+
+        return [row, col]
+
+    def index2address(self, row, col):
+        """
+        Converts index row/col to excel address
+        :param int row: row index (starting at 1)
+        :param int col: col index (start at 1 that corresponds to column "A")
+        :return: str excel address
+        """
+        if type(row) is not int and type(row) is not float:
+            raise ValueError('Error - Incorrect row ({}) entry. Row must either be a int or float'.format(row))
+        if type(col) is not int and type(col) is not float:
+            raise ValueError('Error - Incorrect col ({}) entry. Col must either be a int or float'.format(col))
+        if row <= 0 or col <= 0:
+            raise ValueError('Error - Row ({}) and Col ({}) entry cannot be less than 1'.format(row, col))
+
+        colname = chr(col+64)
+        return colname + str(row)
 
 
 def readxl(fn):
     """
     Reads an xlsx or xlsm file and returns a pylightxl database
     :param str fn: Excel file name
-    :return: pylightxl database class
+    :return: pylightxl.Database class
     """
 
     # declare a db
     db = Database()
+
+    # test that file entered was a valid excel file
+    check_excelfile(fn)
 
     # zip up the excel file to expose the xml files
     with zipfile.ZipFile(fn, 'r') as f_zip:
@@ -65,7 +247,6 @@ def readxl(fn):
         # get custom sheetnames
         with f_zip.open('xl/workbook.xml', 'r') as f:
             sheetnames = get_sheetnames(f)
-            _ = [db.worksheet.update({sheetname: {}}) for sheetname in sheetnames]
 
         # get all of the zip'ed xml sheetnames
         zip_sheetnames = get_zipsheetnames(f_zip)
@@ -80,9 +261,30 @@ def readxl(fn):
         # scrape each sheet#.xml file
         for i, zip_sheetname in enumerate(zip_sheetnames):
             with f_zip.open(zip_sheetname, 'r') as f:
-                db.worksheet[sheetnames[i]].update(scrape_worksheetxml(f, sharedString))
+                db.add_worksheet(sheetname=sheetnames[i], data=scrape_worksheetxml(f, sharedString))
 
     return db
+
+
+def check_excelfile(fn):
+    """
+    Takes a file-path and raises error if the file is not found/unsupported.
+    :param str fn: Excel file path
+    :return: None
+    """
+
+    if type(fn) is not str:
+        raise ValueError('Error - Incorrect file entry ({}).'.format(fn))
+
+    if not isfile(fn):
+        raise ValueError('Error - File ({}) does not exit.'.format(fn))
+
+    extension = fn.split('.')[-1]
+
+    if extension not in ['xlsx', 'xlsm']:
+        raise ValueError('Error - Incorrect Excel file extension ({}). '
+                         'File extension supported: .xlsx .xlsm'.format(extension))
+
 
 def get_sheetnames(file):
     """
@@ -181,14 +383,6 @@ def scrape_worksheetxml(file, sharedString=None):
 
     return data
 
-def cellAddress_to_cellRowCol(cellAddress):
-    """
-    Takes an excel cell address (ie. "A1") and returns a list of the equivalent row/col
-    :param str cellAddress: excel cell address (ie. "A1")
-    :return: list of [row,col]
-    """
-
-    pass
 
 
 
